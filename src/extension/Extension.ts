c95616c75ac9d40c4132e0539f4e951eadb65cbe
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2025-02-02 22:53:35 +0100

    escape : in regexp

--- src/extension/Extension.ts
+++ src/extension/Extension.ts
@@ -243,7 +243,7 @@ function isHostPermission(permission: string): boolean {
     return true;
   }
 
-  return /:\/\//.test(permission);
+  return /\:\/\//.test(permission);
 }
 
 export type PermissionsInfo = {
